cbde195c75369b3938f8ed67e369bfbb550ae59b
Ashley Ahn <ashleyjsahn@gmail.com>
2020-04-10 14:04:22 -0700

    slice 5

--- src/App.js
+++ src/App.js
@@ -38,9 +38,33 @@ const Banner = ({ title }) => (
   <Title>{ title || '[loading...]' }</Title>
 );
 
-const CourseList = ({ courses }) => (
-  <Button.Group>
-    { courses.map(course => <Course key = {course.id} course={course} />) }
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const termCourses = courses.filter(course => term === getCourseTerm(course));
+  return (
+    <React.Fragment>
+      <TermSelector state={{term, setTerm}}/>
+      <Button.Group>
+        { termCourses.map(course => <Course key={course.id} course={course} state={{term, setTerm}} />)}
+      </Button.Group>
+    </React.Fragment>
+  );
+};
+
+const buttonColor = selected => (
+  selected ? 'success' : null
+);
+
+const TermSelector = ({ state }) => (
+  <Button.Group hasAddons>
+  { Object.values(terms).map(value => 
+        <Button key={value}
+          color={buttonColor(value === state.term)}
+          onClick={() => state.setTerm(value)}>
+          {value}
+        </Button>
+      )
+  }
   </Button.Group>
 );
 
