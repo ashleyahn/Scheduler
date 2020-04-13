2a8cb70e85964cfa2847fa77e226b1a43af5134a
Ashley Ahn <ashleyjsahn@gmail.com>
2020-04-13 14:13:16 -0700

    slice 6

--- src/App.js
+++ src/App.js
@@ -2,37 +2,22 @@ import React from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 import { useState, useEffect } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": [
-    {
-      "id": "F101",
-      "title": "Computer Science: Concepts, Philosophy, and Connections",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F110",
-      "title": "Intro Programming for non-majors",
-      "meets": "MWF 10:00-10:50"
-    },
-    {
-      "id": "F111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "W111",
-      "title": "Fundamentals of Computer Programming I",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F211",
-      "title": "Fundamentals of Computer Programming II",
-      "meets": "TuTh 12:30-13:50"
-    }
-  ]
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
+  storageBucket: "....",
+  messagingSenderId: "...",
+  appId: "..."
 };
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
+
 
 const Banner = ({ title }) => (
   <Title>{ title || '[loading...]' }</Title>
